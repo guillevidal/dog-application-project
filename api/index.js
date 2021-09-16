889512c4d3e0f7965bfa0283ed516a2a6ce8eb57
guillevidal <guillermogv150@gmail.com>
2021-09-16 17:05:21 -0300

    listo el back

--- api/index.js
+++ api/index.js
@@ -18,12 +18,22 @@
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const axios = require("axios");
-const { Raza } = require("./src/db.js");
+const { Temperamentos } = require("./src/db.js");
 const server = require("./src/app.js");
+const { API_KEY } = process.env;
 const { conn } = require("./src/db.js");
 // aca configuramos todo
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: true }).then(async () => {
+  let dogsApi = await axios.get(
+    `https://api.thedogapi.com/v1/breeds?api_key=${API_KEY}`
+  );
+  await Temperamentos.bulkCreate(
+    dogsApi.data &&
+      dogsApi.data.map((e) => ({
+        name: e.temperament,
+      }))
+  );
   server.listen(3001, () => {
     console.log("%s listening at 3001"); // eslint-disable-line no-console
   });
